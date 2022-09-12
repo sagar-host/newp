e4a6d238a343fb2fdc83f1904b250be434f14f3e
sagar <92850073+sagar-host@users.noreply.github.com>
2022-09-12 06:45:33 +0530

    Update tool2.js

--- src/Components/Tool1/tool2.js
+++ src/Components/Tool1/tool2.js
@@ -5,7 +5,7 @@ export default function EmbeddedPostExample() {
   return (
     <FacebookProvider appId="421048223348918">
    
-      <EmbeddedPost href="https://www.facebook.com/photo/?fbid=2998117693848601&set=a.1377478522579201" width="500" />
+      <EmbeddedPost href="https://www.facebook.com/photo/?fbid=2998117693848601&set=a.1377478522579201" width="300" />
     
     </FacebookProvider>
   );
